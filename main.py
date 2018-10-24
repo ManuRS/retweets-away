a96248b6db4615a9fa051a02720bf5027313ee07
Manuel Reyes-Sanchez <mnrs94@gmail.com>
2018-10-24 16:26:26 +0200

    Complete refactor
    Now like a real programmer

--- main.py
+++ main.py
@@ -2,105 +2,151 @@ import csv
 import shutil, os
 import argparse
 
-file='tweets.csv'
-file_all='tweets_all.csv'
-file_new='tweets_clear.csv'
+############
+# Variables
+############
+file     = 'tweets.csv'
+file_all = 'tweets_all.csv'
+file_new = 'tweets_clear.csv'
 
-# To create the url
-column_url=0
-# Time of creation
-column_dt=3
-# Tweet text
-column_tw=5
-# Original user ID if is a rtweet
-column_rt=6
+column_url = 0 # To create the url
+column_dt  = 3 # Time of creation
+column_tw  = 5 # Tweet text
+column_rt  = 6 # Original user ID if is a rtweet
 
-user='nick'
+BLUE   = '\033[94m'
+GREEN  = '\033[92m'
+RED    = '\033[91m'
+YELLOW = '\033[93m'
+RESET  = '\033[0m'
 
+line = '\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+
+############
+# Parser
+############
 ap = argparse.ArgumentParser()
+ap.add_argument('-u', '--user', type=str, required=True, help='nickname', default='')
 ap.add_argument('-d', '--date', type=str, required=False, help='yyyy-mm-dd', default='')
-ap.add_argument('-i', '--include', nargs='+', default=[], type=str, required=False, help='[str1,str2,etc]')
-ap.add_argument('-ni', '--ninclude', nargs='+', default=[], type=str, required=False, help='[str1,str2,etc]')
+ap.add_argument('-i', '--include', nargs='+', default=[], type=str, required=False, help='str1 str2 etc')
+ap.add_argument('-ni','--ninclude', nargs='+', default=[], type=str, required=False, help='str1 str2 etc')
 
 args = vars(ap.parse_args())
+user = args["user"]
 date = args["date"]
 include = args["include"]
 ninclude = args["ninclude"]
 
-if date!='':
-	date_on = True
-else:
-	date_on = False
+date_on=True if date!='' else False
 
-print('\033[92mThis script deletes a row if the selected column of that row has content')
-print('Created to delete the re-tweets from an official twitter archive csv\n')
-
-print('Reading from       =', file)
-print('Backup file route  =', file_all)
-print('New file route     =', file_new)
-print('Columns & user     = set in the code\033[0m\n')
-
+############
+# Backup
+############
 if os.path.exists(file):
     with open(file, 'rb') as forigen:
         with open(file_all, 'wb') as fdestino:
             shutil.copyfileobj(forigen, fdestino)
 
+############
+# Filters
+############
+def filters(row, date):
+	# Date filter
+	if (date in row[column_dt]) == False:
+		return False
+
+	# RT filter
+	if row[column_rt]!='': 
+		return False
+
+	# To be done: Included and not included should be without url in the text
+
+	# Included words filter
+	aux=True
+	if len(include)>0:
+		aux=False
+		for elem in include:
+			if row[column_tw].find(elem) != -1:
+				aux=True
+				break
+	if aux==False:
+		return False
+
+	# Non included words filter
+	if len(ninclude)>0:
+		for elem in ninclude:
+			if row[column_tw].find(elem) != -1:
+				return False
+	return True
+
+
+############
+# Main part
+############
 counter=-1
 counter_org=0
 
-f = open('salida.txt', 'w') 
-with open(file_new, 'w', newline='') as csvdest:
-	spamwriter = csv.writer(csvdest, delimiter=',', quoting=csv.QUOTE_ALL)
+# Opening the place to save the tweets
+f       = open('salida.txt', 'w') # File to write in plain text
+csvdest = open(file_new, 'w', newline='')
+csvfile = open(file, newline='')
+spamwriter = csv.writer(csvdest, delimiter=',', quoting=csv.QUOTE_ALL)
+spamreader = csv.reader(csvfile, delimiter=',')
 
-	with open(file, newline='') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',') #quotechar='"'
+for row in spamreader:
+	# First row is the column description
+	if counter<0:
+		txt='\nColumn description:\n'+str(row)
+		print(YELLOW + txt + RESET + line)
+		f.write(txt + line + '\n')
 
-		for row in spamreader:
-			if counter>=0:
-				
-				if date in row[column_dt]: # Case with no date works because '' in any_str is True
-					if row[column_rt]=='': 
-						
-						# Included words
-						filter_a_pass=False
-						if len(include)>0:
-							for elem in include:
-								if row[column_tw].find(elem)!=-1:
-									filter_a_pass=True
-									break
-						else:
-							filter_a_pass=True
-							
-						# Non included words
-						filter_b_pass=True
-						if len(ninclude)>0:
-							for elem in ninclude:
-								if row[column_tw].find(elem)==-1:
-									filter_b_pass=False
-									break
-						# Filters
-						if filter_a_pass and filter_b_pass:
-							#if (row[column_tw].find(include)!=-1) and (row[column_tw].find(ninclude)==-1):
-							# Original tweet
-							counter_org+=1;
-							spamwriter.writerow(row)
+	# Tweet
+	if filters(row, date)==True and counter>=0:
+		counter_org+=1;
+		spamwriter.writerow(row)
 
-							print(row[column_tw])
-							f.write(row[column_tw]) 
+		txt = '\nhttps://twitter.com/'+user+'/statuses/'+str(row[column_url])
+		print(row[column_tw] + BLUE + txt + RESET + line)
+		f.write(row[column_tw] + txt + line + '\n') 
 
-							print('\033[94mhttps://twitter.com/'+user+'/statuses/'+str(row[column_url])+'\033[0m')
-							f.write('\nhttps://twitter.com/'+user+'/statuses/'+str(row[column_url])) 
+	# Count tweets
+	counter+=1; 
 
-							print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-							f.write('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n') 
-				counter+=1; # total counter
+############
+# Final text
+############
+txt  = 'Reading from       = ' + file
+txt += '\nBackup file route  = ' + file_all
+txt += '\nNew file route     = ' + file_new + '\n'
 
-			else: # Only 1st time
-				print('\033[93mColumn description:\n'+str(row)+'\033[0m')
-				print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-				f.write('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
-				counter+=1;
+print(GREEN + txt)
+f.write(txt + '\n')
 
-print('\033[92mTotal number of tweets (with rt)    = ', counter)
-print('Original (and filtered) tweets      = ', counter_org)
-print('Original (and filtered) tweets %    =  {0:.2f}%\033[0m'.format(counter_org*100/counter))
+txt  = 'user      = ' + user
+txt += '\ndate      = ' + date
+txt += '\ninclude   = ' + str(include)
+txt += '\nninclude  = ' + str(ninclude) + '\n'
+
+print(RED + txt)
+f.write(txt + '\n')
+
+txt  = 'Total # of tweets (with RT)  = ' + str(counter)
+txt += '\nFiltered tweets              = ' + str(counter_org)
+txt += '\nFiltered tweets %            = {0:.2f}%\n'.format(counter_org*100/counter)
+
+print(GREEN + txt)
+f.write(txt + '\n')
+
+txt  = 'Created to delete re-tweets from a csv twitter archive'
+txt += '\nNow you can also filter by date or words'
+url  = '\nhttps://github.com/manurs/retweets-away\n'
+
+print(RESET + txt + BLUE + url + RESET)
+f.write(txt + url)
+
+############
+# Close
+############
+f.close()
+csvdest.close()
+csvfile.close()
